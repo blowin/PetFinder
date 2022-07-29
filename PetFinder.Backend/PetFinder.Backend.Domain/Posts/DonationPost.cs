6a4f5cc9a577b77c5cc271af4d5d4455b7f8bc69
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-07-29 19:44:15 +0300

    Backend, Domain - Move common fields to Post class

--- PetFinder.Backend/PetFinder.Backend.Domain/Posts/DonationPost.cs
+++ PetFinder.Backend/PetFinder.Backend.Domain/Posts/DonationPost.cs
@@ -4,13 +4,9 @@ namespace PetFinder.Backend.Domain.Posts;
 
 public class DonationPost : Post
 {
-    public string Name { get; private set; }
-    public string Description { get; private set; }
     public Pet Pet { get; private set; }
-    public Guid CreatedUserId { get; private set; }
     
     public Address? Address { get; set; }
-    public DateTime? CloseDate { get; set; }
 
     private DonationPost()
         : this(string.Empty, string.Empty, Guid.Empty, new Pet(PetType.Cat), new List<Photo>())
@@ -18,11 +14,8 @@ public class DonationPost : Post
     }
     
     public DonationPost(string name, string description, Guid createdUserId, Pet pet, List<Photo> photos)
-        : base(photos)
+        : base(name, description, photos, createdUserId)
     {
-        Name = name;
-        Description = description;
         Pet = pet;
-        CreatedUserId = createdUserId;
     }
 }
